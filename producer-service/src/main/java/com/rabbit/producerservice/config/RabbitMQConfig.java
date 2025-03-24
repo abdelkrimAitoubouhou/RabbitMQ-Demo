bba8d098794c9351aca68065d7ed1187e9e658ac
abdelkrimAitoubouhou <aitoubouhoukarim@gmail.com>
2025-03-24 04:02:56 +0000

    first commit

---
+++ producer-service/src/main/java/com/rabbit/producerservice/config/RabbitMQConfig.java
@@ -0,0 +1,17 @@
+package com.rabbit.producerservice.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "firstQueue";
+
+    @Bean
+    public Queue exampleQueue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+}
---
+++ producer-service/src/main/java/com/rabbit/producerservice/service/MessageProducer.java
@@ -0,0 +1,19 @@
+package com.rabbit.producerservice.service;
+
+import com.rabbit.producerservice.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+            rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
+    }
+}
