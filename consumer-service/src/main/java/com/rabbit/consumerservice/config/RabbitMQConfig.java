f0604bad622d7583bec2bb7e7c24be3abb6291c2
abdelkrimAitoubouhou <aitoubouhoukarim@gmail.com>
2025-03-25 17:04:32 +0000

    add SMTP server and handle sending mails from the queue

--- consumer-service/src/main/java/com/rabbit/consumerservice/config/RabbitMQConfig.java
+++ consumer-service/src/main/java/com/rabbit/consumerservice/config/RabbitMQConfig.java
@@ -1,5 +1,6 @@
 package com.rabbit.consumerservice.config;
 
+import com.rabbit.consumerservice.service.EmailService;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    private final EmailService emailService;
+
+
+    public RabbitMQConfig(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+
     public static final String QUEUE_NAME = "myQueue";
+
 
     @Bean
     public Queue exampleQueue() {
@@ -18,5 +28,13 @@ public class RabbitMQConfig {
     @RabbitListener(queues = QUEUE_NAME)
     public void listen(String message) {
         System.out.println("Received message: " + message);
+
+        // Send email when message is received
+        emailService.sendEmail(
+                "aitoubouhouhafid@gmail.com",
+                "Rabbit msg",   // Email Subject
+                message                   // Email Body (Message from RabbitMQ)
+        );
+
     }
 }
---
+++ consumer-service/src/main/java/com/rabbit/consumerservice/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.rabbit.consumerservice.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailMessage.setFrom("your_email@gmail.com"); // Sender email
+
+        mailSender.send(mailMessage);
+        System.out.println("Email sent successfully to " + to);
+    }
+}
